b339bee47d8c712508ad330ceff4437d7b4a868a
MorawskiR <r.morawski92@gmail.com>
2021-11-10 22:05:34 +0100

    first commit

---
+++ Car.hpp
@@ -0,0 +1,14 @@
+
+
+class Car{
+
+
+public: 
+    void turnLeft();
+    void turnRight();
+    void brake();
+    void accelerate(int speed);
+    void charge();
+    void refuel();
+
+};
---
+++ Engine.hpp
@@ -0,0 +1,9 @@
+
+class Engine
+{
+    int power_;             // in HP 
+    int batteryCapacity_;   // in Ah
+    float capacity_;    // in ccm
+    int gears_;
+    int currentGear_;
+};
